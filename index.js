8d3aa9689758c999a4a2953a03c04d30a86fea54
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2022-12-06 22:38:32 -0300

    scroll background

--- index.js
+++ index.js
@@ -39,7 +39,29 @@ class Player {
   }
 }
 
+class Platform {
+  constructor({ x, y }) {
+    this.position = {
+      x,
+      y,
+    };
+    this.width = 200;
+    this.height = 30;
+  }
+
+  draw() {
+    c.fillStyle = "blue";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+
 const player = new Player();
+const platforms = [
+  new Platform({ x: 200, y: 500 }),
+  new Platform({ x: 500, y: 450 }),
+  new Platform({ x: 800, y: 550 }),
+];
+
 const keys = {
   right: {
     pressed: false,
@@ -49,16 +71,51 @@ const keys = {
   },
 };
 
+let scrollOffset = 0;
+
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
   player.update();
+  platforms.forEach((platform) => {
+    platform.draw();
+  });
 
-  if (keys.right.pressed) {
+  if (keys.right.pressed && player.position.x < 500) {
     player.velocity.x = 5;
-  } else if (keys.left.pressed) {
+  } else if (keys.left.pressed && player.position.x > 100) {
     player.velocity.x = -5;
-  } else player.velocity.x = 0;
+  } else {
+    player.velocity.x = 0;
+    if (keys.right.pressed) {
+      scrollOffset += 5;
+      platforms.forEach((platform) => {
+        platform.position.x -= 5;
+      });
+    } else if (keys.left.pressed) {
+      scrollOffset -= 5;
+      platforms.forEach((platform) => {
+        platform.position.x += 5;
+      });
+    }
+  }
+
+  // Platform collision detection
+  platforms.forEach((platform) => {
+    if (
+      player.position.y + player.height <= platform.position.y &&
+      player.position.y + player.height + player.velocity.y >
+        platform.position.y &&
+      player.position.x + player.width >= platform.position.x &&
+      player.position.x <= platform.position.x + platform.width
+    ) {
+      player.velocity.y = 0;
+    }
+  });
+
+  if (scrollOffset > 2500) {
+    console.log("You win");
+  }
 }
 animate();
 
